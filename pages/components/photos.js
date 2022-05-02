ecea9bd0378ea8b8dd8c8c0a86dfac9e01b06e7b
NasserAlbusaidi <nasserbusaidi@gmail.com>
2022-05-02 22:28:11 +0400

    AAAAAAAAAAAAAAAA

--- pages/components/photos.js
+++ pages/components/photos.js
@@ -1,14 +1,14 @@
 export const images = [
   {
-    src: './1.jpg',
+    src: '/1.jpg',
     text: 'cute cat',
   },
   {
-    src: './2.jpg',
+    src: '/2.jpg',
     text: 'Hello World 2',
   },
   {
-    src: './3.jpg',
+    src: '/3.jpg',
     text: 'Hello World 3',
   },
 ];
