b8d27353de2914df551b53a96183b4e31533f243
NasserAlbusaidi <nasserbusaidi@gmail.com>
2022-05-03 23:12:58 +0400

    i did it

--- pages/components/Grid.js
+++ pages/components/Grid.js
@@ -1,3 +1,5 @@
+import { IoMdLocate, IoIosCalendar } from 'react-icons/io';
+
 export default function GridGalleryCard({
   imageUrl,
   show,
@@ -13,10 +15,20 @@ export default function GridGalleryCard({
     >
       <div className="absolute inset-0 z-10 flex transition duration-200 ease-in hover:opacity-0">
         <div className="absolute inset-0 bg-black opacity-60"></div>
-        <div className="mx-auto text-white z-10 self-center uppercase tracking-widest text-[10px]">
-          <span className=" text-center opacity-50 text-[12px]">{text}</span>
-          {location && <span className="block opacity-50 ">{location}</span>}
-          {date && <span className="block opacity-50">{date}</span>}
+        <div className="mx-auto text-white z-10 self-center uppercase tracking-widest text-[8px]">
+          <span className=" text-center opacity-50 text-[10px] ">{text}</span>
+          {location && (
+            <div className="flex flex-between pt-2 italic">
+              <IoMdLocate size={11} className="mr-1" />
+              <span className=" opacity-50 ">{location}</span>
+            </div>
+          )}
+          {date && (
+            <div className="flex flex-between pt-1 italic ">
+              <IoIosCalendar size={11} className="mr-2" />
+              <span className=" opacity-50 ">{date}</span>
+            </div>
+          )}
         </div>
       </div>
       <img src={imageUrl} alt="hi" />
