00406b2fc36cb9508e7a55f8509b175ba3c01ea4
NasserAlbusaidi <nasserbusaidi@gmail.com>
2022-05-04 17:40:36 +0400

    DB integration

--- pages/components/Nav.js
+++ pages/components/Nav.js
@@ -5,10 +5,12 @@ export default function Nav() {
     <header id="top">
       <nav className="flex sm:py-16 sm:mx-4 py-6 mx-2  font-bold">
         <div className="flex-1">
-          <a href="/">
-            <h1 className="sm:text-2xl text-l uppercase ">[Your Name here]</h1>
-            <h2 className=" text-sm font-thin italic ">Photographer</h2>
-          </a>
+          <h1 className="sm:text-2xl text-l uppercase ">
+            <Link href="/">[Your Name here]</Link>
+          </h1>
+          <h2 className=" text-sm font-thin italic ">
+            <Link href="/">Photographer</Link>
+          </h2>
         </div>
         <ul className="flex justify-end font-extralight py-2 ">
           <li className="sm:mr-6 mr-2 text-xs sm:text-xl  ">
